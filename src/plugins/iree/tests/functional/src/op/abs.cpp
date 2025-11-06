a245107aaa20fd18677121085df78d689d91d696
Krivoruchko, Georgy <georgy.krivoruchko@amd.com>
2025-11-06 17:15:08 +0000

    Fixed Abs tests

--- src/plugins/iree/tests/functional/src/op/abs.cpp
+++ src/plugins/iree/tests/functional/src/op/abs.cpp
@@ -10,16 +10,22 @@ TEST(iree_plugin_tests, op_abs) {
     auto test_suite = ov::iree::OperationChecker();
 
     auto input0 = std::make_shared<ov::op::v0::Parameter>(ov::element::f32, ov::Shape{3});
-    input0->output(0).add_names({"input0"});
-    input0->set_friendly_name("input0");
     auto abs = std::make_shared<ov::op::v0::Abs>(input0);
-    input0->output(0).add_names({"operation"});
-    abs->set_friendly_name("operation");
     auto result0 = std::make_shared<ov::op::v0::Result>(abs);
-    result0->output(0).add_names({"operation"});
-    result0->set_friendly_name("output0");
     test_suite.set_model(std::make_shared<ov::Model>(ov::ResultVector{result0}, ov::ParameterVector{input0}));
 
     test_suite.add_input<float>(ov::Shape{3}, {-1.,0.,1.});
+    test_suite.run();
+}
+
+TEST(iree_plugin_tests, op_abs_rank2) {
+    auto test_suite = ov::iree::OperationChecker();
+
+    auto input0 = std::make_shared<ov::op::v0::Parameter>(ov::element::f32, ov::Shape{3,2});
+    auto abs = std::make_shared<ov::op::v0::Abs>(input0);
+    auto result0 = std::make_shared<ov::op::v0::Result>(abs);
+    test_suite.set_model(std::make_shared<ov::Model>(ov::ResultVector{result0}, ov::ParameterVector{input0}));
+
+    test_suite.add_input<float>(ov::Shape{3,2}, {-1.,0.,1.,100.,-123.,-0.1234f});
     test_suite.run();
 }
