4e138b91c5984663bd7972f281900421e14461bf
Krivoruchko, Georgy <georgy.krivoruchko@amd.com>
2025-11-06 17:47:18 +0000

    Removed complex implementation of Multiply

--- src/plugins/iree/src/compiled_model.cpp
+++ src/plugins/iree/src/compiled_model.cpp
@@ -12,6 +12,7 @@ namespace iree {
 static std::mutex translators_mutex;
 static std::map<std::string, std::string> ov_to_aten = {
     {"Abs", "torch.aten.abs"},
+    {"Multiply", "torch.aten.mul.Tensor"},
     {"Subtract", "torch.aten.sub"},
     {"Divide", "torch.aten.div"},
     {"Power", "torch.aten.pow"},
