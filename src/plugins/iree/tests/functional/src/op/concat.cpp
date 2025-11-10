5b6087d120a5007ecc207d0cee345582b6288872
Maria <landwife2508+github@gmail.com>
2025-11-10 19:57:02 +0400

    Added test for Concat

---
+++ src/plugins/iree/tests/functional/src/op/concat.cpp
@@ -0,0 +1,22 @@
+#include "openvino/op/concat.hpp"
+
+#include "openvino/core/model.hpp"
+#include "openvino/op/constant.hpp"
+#include "openvino/op/parameter.hpp"
+#include "openvino/op/result.hpp"
+#include "openvino/runtime/infer_request.hpp"
+#include "operation_checker.hpp"
+
+TEST(iree_plugin_tests, op_concat_tensor_tensor) {
+    auto test_suite = ov::iree::OperationChecker();
+
+    auto input0 = std::make_shared<ov::op::v0::Parameter>(ov::element::f32, ov::Shape{3});
+    auto input1 =
+        std::make_shared<ov::op::v0::Constant>(ov::element::f32, ov::Shape{3}, std::vector<float>{1., 2., 3.});
+    auto concat = std::make_shared<ov::op::v0::Concat>(ov::NodeVector{input0, input1}, /*axis=*/0);
+    auto result0 = std::make_shared<ov::op::v0::Result>(concat);
+    test_suite.set_model(std::make_shared<ov::Model>(ov::ResultVector{result0}, ov::ParameterVector{input0}));
+
+    test_suite.add_input<float>(ov::Shape{3}, {-1., 0., 1.});
+    test_suite.run();
+}
